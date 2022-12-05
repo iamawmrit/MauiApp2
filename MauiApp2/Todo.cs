0bdd603b26b73371854c4675a9b6acb6d2ffc4a7
iamawmrit <awmrit@gmail.com>
2022-12-05 07:52:35 +0545

    first commit

---
+++ MauiApp2/Todo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiApp2
+{
+    internal class Todo
+    {
+        public string todoText { get; set; }
+        public bool IsComplected { get; set; }
+    }
+}
