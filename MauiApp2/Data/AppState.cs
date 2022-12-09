d67124c328d2ba7ce5ffaeca0acfd87a9ad8a8b5
iamawmrit <awmrit@gmail.com>
2022-12-09 08:01:48 +0545

    navigation  to loginPage

---
+++ MauiApp2/Data/AppState.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiApp2.Data
+{
+    internal class AppState
+    {
+        public static bool IsLoggedIn { get; set; }
+    }
+}
